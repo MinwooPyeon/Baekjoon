92abcf5781f637f2db5a02143828d469ac573143
Las <153968515+MinwooPyeon@users.noreply.github.com>
2025-05-15 09:24:31 +0900

    [Gold IV] Title: 저울, Time: 112 ms, Memory: 13544 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.java
+++ 백준/Gold/10159. 저울/저울.java
@@ -2,52 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int N, M;
+	static ArrayList<Integer>[] graph;
+	static boolean[] isVisited;
+	static int count;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		
-		int[][] arr = new int[n+1][n+1];
-		for(int i=1; i<n+1; i++) {
-			arr[i][i] = 1;
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		graph = new ArrayList[N + 1];
+		isVisited = new boolean[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
 		}
-		for(int i=0; i<m; i++) {
-			 st = new StringTokenizer(br.readLine());
-			 int a1 = Integer.parseInt(st.nextToken());
-			 int a2 = Integer.parseInt(st.nextToken());
-			 
-			 arr[a1][a2]=1; // a1 > a2;
-			 arr[a2][a1]= -1; // a2 > a1;
+		StringTokenizer st;
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			graph[b].add(a * -1);
 		}
-		
-		for(int k=1; k<n+1; k++) {
-			for(int i=1; i<n+1; i++) {
-				for(int j=1; j<n+1; j++) {
-					if(arr[i][k] == 1 && arr[k][j] ==1) {
-						arr[i][j] =1;
-					}
-					
-					if(arr[i][k] == -1 && arr[k][j] == -1) {
-						arr[i][j] = -1;
-					}
-				}
+
+		for (int i = 1; i <= N; i++) {
+			count = N;
+			Arrays.fill(isVisited, false);
+			dfs(i);
+			isVisited[i] = false;
+			count++;
+			reverseDfs(i);
+			System.out.println(count);
+		}
+	}
+
+	static void dfs(int n) {
+		if (isVisited[n])
+			return;
+
+		isVisited[n] = true;
+		count--;
+
+		for (int node : graph[n]) {
+			if (node > 0 && !isVisited[node]) {
+				dfs(node);
 			}
 		}
-		
-		for(int i=1; i<n+1; i++) {
-			int cnt=0;
-			for(int j=1; j<n+1; j++) {
-				if(arr[i][j] != 0) {
-					cnt++;
-				}
+	}
+	static void reverseDfs(int n) {
+		if (isVisited[n])
+			return;
+
+		isVisited[n] = true;
+		count--;
+
+		for (int node : graph[n]) {
+			if (node < 0 && !isVisited[node * -1]) {
+				reverseDfs(node * -1);
 			}
-			sb.append((n-cnt)+"\n");
-			
 		}
-		System.out.println(sb.toString());
 	}
 }
